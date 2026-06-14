a9379dba1ec02470f16bb65bcb31410d41a1c2b2
Chinmay Panda <chinmay@openautonomyx.com>
2026-06-14 07:01:02 -0700

    mac: expose allowlisted Fabric Core actions to app

--- fabric-mac-core/src-tauri/src/main.rs
+++ fabric-mac-core/src-tauri/src/main.rs
@@ -1,7 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod actions;
+
+#[tauri::command]
+fn list_fabric_core_actions() -> Result<actions::FabricCoreActions, String> {
+    actions::load_actions()
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![list_fabric_core_actions])
         .run(tauri::generate_context!())
         .expect("error while running Fabric Mac Core");
 }
